3755539a8ccdf61011fa11d156c3fc0653c898ad
MARISOL PUNTUSIL ORMACHEA <marisol.pormachea@senacsp.edu.br>
2024-10-07 17:29:16 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// let fruta =["abacate","meláo","kiwi","maça"];
+// console.log(fruta);
+// console.log(fruta[fruta.length -1]); //Último elemento array
+
+// for(let i =0; i < fruta.length; i++) ;{
+//     console.log(fruta[i]);
+// }
+
+
+
+
+//  carros =["Fusca","Variant","Brasilia","Caravan","Opala"]
+
+// for (let i = 0; i < carros.length; 1++) {
+//     console.log(carros[i])};
+// let
+
+
+//  carros = ["Fusca","Variant","Brasília","caramvan","Opalo"];
+
+// //Método map para criar um novo array que contém o comprimento de cada nome de carro
+// let novoCarros = carros.map((carro) => {
+//     return carro.length;
+// });
+// console.letlog(novoCarros); // Exibe o novo array  com os comprimentos
+
+let legumes = ["batata", "cenaura","beterraba","beringela","chuchu"]
+let legumesComB = legumes.filter(legume => legume.
+    startsWith("b"));
+
+    console.log(legumesComB);
